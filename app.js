53a0f8b7d04ed90416dbf473f67a813bbd4ebad4
canned-ice <torenesk@gmail.com>
2018-07-24 12:14:43 -0700

    fixing git

--- app.js
+++ app.js
@@ -3,56 +3,61 @@
 var imgs = ['bag.jpg', 'banana.jpg', 'bathroom.jpg', 'boots.jpg', 'breakfast.jpg', 'bubblegum.jpg', 'chair.jpg', 'cthulhu.jpg', 'dog-duck.jpg', 'dragon.jpg', 'pen.jpg', 'pet-sweep.jpg', 'scissors.jpg', 'shark.jpg', 'sweep.png', 'tauntaun.jpg', 'unicorn.jpg', 'usb.gif', 'water-can.jpg', 'wine-glass.jpg'];
 var imgObjects = [];
 var noDupes = [];
+var imgLeft = document.getElementById('left');
+var imgMid = document.getElementById('mid');
+var imgRight = document.getElementById('right');
 var imgOne = 0;
 var imgTwo = 0;
-var imgThree = 0;
+var imgThr = 0;
 var section = document.getElementById('display');
+var totalClicks = 0;
 
 function ImageTracker(img) {
   this.name = img.split('.')[0];
-  this.path = 'img/' + img;
+  this.path = `img/${img}`;
   this.totalDisplays = 0;
-  this.totalClicks = 0;
+  this.allClicks = 0;
 }
 
 for (var i = 0; i < imgs.length; i++) {
   imgObjects.push(new ImageTracker(imgs[i]));
 }
-// ImageTracker
+// ImageTracker structure
 //   name: 'name'
 //   path: 'name.jpg'
+//   totalDisplays: 0
 //   totalClicks: 0
 
-var randomNum = function(max) { // creates a random number
-  return Math.floor(Math.random() * Math.floor(max));
+var randomNum = function() { // creates a random number
+  return Math.floor(Math.random() * Math.floor(imgObjects.length));
 };
-console.log(randomNum(imgObjects.length));
+console.log(randomNum());
 
 var getImageOne = function() {
-  imgOne = randomNum(imgObjects.length);
+  imgOne = randomNum();
   if (noDupes.includes(imgOne)) {
-    imgOne = randomNum(imgObjects.length);
+    imgOne = randomNum();
     return getImageOne();
   } else {
     noDupes.push(imgOne);
   }
 };
 var getImageTwo = function() {
-  imgTwo = randomNum(imgObjects.length);
+  imgTwo = randomNum();
   if (noDupes.includes(imgTwo)) {
-    imgTwo = randomNum(imgObjects.length);
+    imgTwo = randomNum();
     return getImageTwo();
   } else {
     noDupes.push(imgTwo);
   }
 };
 var getImageThree = function() {
-  imgThree = randomNum(imgObjects.length);
-  if (noDupes.includes(imgThree)) {
-    imgThree = randomNum(imgObjects.length);
+  imgThr = randomNum();
+  if (noDupes.includes(imgThr)) {
+    imgThr = randomNum();
     return getImageThree();
   } else {
-    noDupes.push(imgThree);
+    noDupes.push(imgThr);
   }
 };
 
@@ -63,21 +68,40 @@ var resetDupes = function() { // reset noDupes function to remove first 3 in arr
 };
 
 var createImgs = function() {
-  var imgOneEl = document.createElement('img');
   var imgUrl = imgObjects[imgOne].path;
-  imgOneEl.setAttribute('src', imgUrl);
-  section.appendChild(imgOneEl);
-  var imgTwoEl = document.createElement('img');
+  imgLeft.setAttribute('src', imgUrl);
   imgUrl = imgObjects[imgTwo].path;
-  imgTwoEl.setAttribute('src', imgUrl);
-  section.appendChild(imgTwoEl);
-  var imgThreeEl = document.createElement('img');
-  imgUrl = imgObjects[imgThree].path;
-  imgThreeEl.setAttribute('src', imgUrl);
-  section.appendChild(imgThreeEl);
+  imgMid.setAttribute('src', imgUrl);
+  imgUrl = imgObjects[imgThr].path;
+  imgRight.setAttribute('src', imgUrl);
 };
 
 getImageOne();
 getImageTwo();
 getImageThree();
 createImgs();
+
+section.addEventListener('click', handleClick, false);
+
+function handleClick(event) {
+  if (event.target.id === 'container') {
+    return alert('click images');
+  }
+
+  console.log(event.target.title);
+  for(var k =0; k < imgObjects.length; k++) {
+    if (event.target.title === imgObjects[k].name) {
+      imgObjects[k].allClicks++;
+      console.log('imgObjects[k]', imgObjects[k].allClicks);
+    }
+  }
+
+  totalClicks++;
+  console.log(totalClicks, 'total clicks');
+  if (totalClicks > 2) {
+    section.removeEventListener('click', handleClick, false);
+    return alert('no more clicks');
+  }
+  console.log(event.target, 'was clicked');
+  createImgs();
+}
